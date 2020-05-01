43bb77788dca7add9839154489c2c6596c3fae94
AnyaSen <anyasenchikhina18@gmail.com>
2020-05-01 18:17:02 +0300

    Created a get request to read the words list

--- backend/routers/word-router.js
+++ backend/routers/word-router.js
@@ -4,11 +4,21 @@ const router = new express.Router();
 
 router.post("/words", async (req, res) => {
   const word = new Word(req.body);
+
   try {
     await word.save();
     res.status(201).send(word);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+router.get("/words", async (req, res) => {
+  try {
+    const wordsList = await Word.find({});
+    res.send(wordsList);
+  } catch (e) {
+    res.status(500).send();
   }
 });
 
