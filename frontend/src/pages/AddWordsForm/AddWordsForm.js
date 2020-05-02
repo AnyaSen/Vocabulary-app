cd0cb0ab6723d68bf87b22a6818ef8581504e04d
AnyaSen <anyasenchikhina18@gmail.com>
2020-05-02 14:12:29 +0300

    Created a form for adding new words

--- frontend/src/pages/AddWordsForm/AddWordsForm.js
+++ frontend/src/pages/AddWordsForm/AddWordsForm.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { postData } from "../../services/postData";
 
 export default function AddWordsForm() {
+  const [inputWord, setInputWord] = useState("");
+  const [inputTranslation, setInputTranslation] = useState("");
+
+  const handleWordInputChange = event => {
+    setInputWord(event.target.value);
+  };
+
+  const handleTranslationInputChange = event => {
+    setInputTranslation(event.target.value);
+  };
+
+  const handleSubmit = e => {
+    const wordsURL = "/words";
+    e.preventDefault();
+
+    postData(wordsURL, {
+      foreignWord: inputWord,
+      translation: inputTranslation
+    });
+    setInputTranslation("");
+    setInputWord("");
+  };
+
   return (
-    <div>
-      <input placeholder="Foreing word" />
-      <input placeholder="Translation" />
-      <button>Add</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Foreing word"
+        type="text"
+        name="word"
+        value={inputWord}
+        onChange={handleWordInputChange}
+      />
+      <input
+        placeholder="Translation"
+        type="text"
+        name="translation"
+        value={inputTranslation}
+        onChange={handleTranslationInputChange}
+      />
+      <button type="submit" value="submit">
+        Add
+      </button>
+    </form>
   );
 }
