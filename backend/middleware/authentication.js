a109a526edd1a0809a63b5e00c9e29c28bac490f
AnyaSen <anyasenchikhina18@gmail.com>
2020-05-05 11:25:00 +0300

    Set up authentication middleware

---
+++ backend/middleware/authentication.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user-model");
+
+const authentication = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, "thisismysecret");
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token
+    });
+    if (!user) {
+      throw new Error();
+    }
+
+    req.user = user;
+    req.token = token;
+
+    next();
+  } catch (e) {
+    res.status(401).send({ error: "Please Authenticate." });
+  }
+};
+
+module.exports = authentication;
