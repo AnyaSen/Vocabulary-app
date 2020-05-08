08ee967127617f4fb05be2577a9b93816455ead8
AnyaSen <anyasenchikhina18@gmail.com>
2020-05-08 11:13:00 +0300

    Added type and min length properties

--- frontend/src/components/InputField/InputField.js
+++ frontend/src/components/InputField/InputField.js
@@ -2,15 +2,24 @@ import React from "react";
 
 import Styles from "./InputField.module.scss";
 
-export default function InputField({ placeholder, name, value, onChange }) {
+export default function InputField({
+  placeholder,
+  name,
+  value,
+  onChange,
+  type,
+  minlength
+}) {
   return (
     <input
       className={Styles.InputField}
-      type="text"
+      type={type}
       placeholder={placeholder}
       name={name}
       value={value}
       onChange={onChange}
+      required
+      minlength={minlength}
     />
   );
 }
