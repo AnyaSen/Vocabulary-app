eb0a100c568e4b232368d8501038d89c79a4a709
AnyaSen <anyasenchikhina18@gmail.com>
2020-05-12 10:19:21 +0300

    Added delete and update words endpoint

--- backend/routers/word-router.js
+++ backend/routers/word-router.js
@@ -27,4 +27,43 @@ router.get("/words", authentication, async (req, res) => {
   }
 });
 
+router.delete("/words/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const word = await Word.findByIdAndDelete(_id);
+    if (!word) {
+      return res.status(404).send();
+    }
+    res.send(word);
+  } catch {
+    res.status(500).send();
+  }
+});
+
+router.patch("/words/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdtes = ["foreignWord", "translation"];
+  const isValidOperation = updates.every(update =>
+    allowedUpdtes.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const word = await Word.findByIdAndUpdate(req.params.id, req.body);
+    updates.forEach(update => (word[update] = req.body[update]));
+    await word.save();
+
+    if (!word) {
+      return res.status(404).send();
+    }
+    res.send(word);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 module.exports = router;
