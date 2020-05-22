362e208b0f241ca07f9f976c1a40228dee22990a
AnyaSen <anyasenchikhina18@gmail.com>
2020-05-22 10:58:52 +0300

    Added styles to NavLinks

--- frontend/src/components/Buttons/ButtonMenuLink/ButtonMenuLink.js
+++ frontend/src/components/Buttons/ButtonMenuLink/ButtonMenuLink.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import Styles from "./ButtonMenuLink.module.scss";
 
 export default function ButtonMenuLink({ buttonMessage, linkTo }) {
   return (
-    <Link to={linkTo} className={Styles.ButtonMenuLink}>
+    <NavLink
+      exact
+      activeClassName={Styles.active}
+      to={linkTo}
+      className={Styles.ButtonMenuLink}
+    >
       <button>{buttonMessage}</button>
-    </Link>
+    </NavLink>
   );
 }
