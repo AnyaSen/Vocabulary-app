6bdb484fb423e7aeba7982ee92700b8a062eed2d
AnyaSen <anyasenchikhina18@gmail.com>
2020-05-23 16:14:56 +0300

    Improved validation

--- frontend/src/pages/SignUpPage/SignUpPage.js
+++ frontend/src/pages/SignUpPage/SignUpPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 
+import Styles from "../../components/SignupLoginForm/SignupLoginForm.module.scss";
+
 import { signup } from "../../services/signup";
 import { ErrorContext } from "../../contexts/ErrorContext";
 import { LoadingContext } from "../../contexts/LoadingContext";
@@ -13,6 +15,9 @@ export default function SignUpPage() {
   const [inputName, setInputName] = useState("");
   const [inputEmail, setInputEmail] = useState("");
   const [inputPassword, setInputPassword] = useState("");
+  const [isEmptyInputError, setIsEmptyInputError] = useState(false);
+  const [isPasswordError, setIsPasswordError] = useState(false);
+  const [isEmailError, setIsEmailError] = useState(false);
 
   const { isSignupError, setIsSignupError } = useContext(ErrorContext);
   const { isProfileLoading, setIsProfileLoading } = useContext(LoadingContext);
@@ -31,18 +36,17 @@ export default function SignUpPage() {
     setInputPassword(event.target.value);
   };
 
-  const handleSubmit = async event => {
-    event.preventDefault();
-    setIsProfileLoading(true);
-
+  const sendProfile = async () => {
     try {
-      setIsSignupError(false);
+      setIsProfileLoading(true);
+
       await signup({
         name: inputName,
         email: inputEmail,
         password: inputPassword
       });
-      history.push("/vocabulary");
+
+      history.push("/home");
 
       setInputName("");
       setInputEmail("");
@@ -57,6 +61,43 @@ export default function SignUpPage() {
     }
   };
 
+  const validateEmail = () => {
+    // eslint-disable-next-line
+    const regexp = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+    return regexp.test(String(inputEmail).toLowerCase());
+  };
+
+  const isValidEmail = validateEmail();
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    setIsSignupError(false);
+
+    if (inputName === "" || inputEmail === "" || inputPassword === "") {
+      setIsPasswordError(false);
+      setIsEmailError(false);
+
+      setIsEmptyInputError(true);
+    } else if (!isValidEmail) {
+      setIsEmptyInputError(false);
+      setIsPasswordError(false);
+
+      setIsEmailError(true);
+    } else if (inputPassword.length < 6 && inputPassword.length > 0) {
+      setIsEmptyInputError(false);
+
+      setIsPasswordError(true);
+    } else {
+      setIsEmptyInputError(false);
+      setIsPasswordError(false);
+      setIsEmailError(false);
+
+      sendProfile();
+    }
+  };
+
   if (isProfileLoading) return <LoadingPage />;
 
   return (
@@ -68,7 +109,15 @@ export default function SignUpPage() {
       route="/login"
       linkMessage="Log In"
     >
-      {isSignupError ? <p>Account already exists</p> : null}
+      {isSignupError ? (
+        <p className={Styles.error}>Error</p>
+      ) : isEmptyInputError ? (
+        <p className={Styles.error}> Please, fill in all the fields</p>
+      ) : isPasswordError ? (
+        <p className={Styles.error}> Password should be 6 characters</p>
+      ) : isEmailError ? (
+        <p className={Styles.error}> Invalid email</p>
+      ) : null}
 
       <InputField
         type="text"
