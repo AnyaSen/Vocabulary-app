17dc2640d0c682fdb3711db5cfcad55660a8fa1b
AnyaSen <anyasenchikhina18@gmail.com>
2020-06-04 14:49:18 +0300

    Added new states for the learning page

--- frontend/src/pages/LearningPage/QuestionPage/QuestionPage.js
+++ frontend/src/pages/LearningPage/QuestionPage/QuestionPage.js
@@ -1,14 +1,49 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+
+import { getRandomWords } from "../../../services/getRandomWords";
 
 import { LearningContext } from "../../../contexts/LearningContext";
+import { WordsContext } from "../../../contexts/WordsContext";
 
 export default function QuestionPage() {
   const {
     totalWordsNumber,
     newWordsNumber,
     learningWordsNumber,
-    learnedWordsNumber
+    learnedWordsNumber,
+    setTotalWordsArray,
+    setNewWordsArray,
+    setLearningWordsArray,
+    setLearnedWordsArray
   } = useContext(LearningContext);
+
+  const { newWords, learningWords, learnedWords, setWordsData } = useContext(
+    WordsContext
+  );
+
+  const randomNewWordsArr = getRandomWords(newWords, newWordsNumber);
+  const randomLearningWordsArr = getRandomWords(
+    learningWords,
+    learningWordsNumber
+  );
+  const randomLearnedWordsArr = getRandomWords(
+    learnedWords,
+    learnedWordsNumber
+  );
+
+  const totalArray = randomNewWordsArr.concat(
+    randomLearningWordsArr,
+    randomLearnedWordsArr
+  );
+
+  useEffect(() => {
+    setWordsData();
+    setTotalWordsArray(totalArray);
+    setNewWordsArray(randomNewWordsArr);
+    setLearningWordsArray(randomLearningWordsArr);
+    setLearnedWordsArray(randomLearnedWordsArr);
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <div>
