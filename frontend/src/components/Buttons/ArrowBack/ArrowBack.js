dbf7eb9b09144a88acf6bbb77e2440faf73154d7
AnyaSen <anyasenchikhina18@gmail.com>
2020-06-05 14:41:22 +0300

    Added a mixin for position absolute and Modified scss variables

--- frontend/src/components/Buttons/ArrowBack/ArrowBack.js
+++ frontend/src/components/Buttons/ArrowBack/ArrowBack.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import Styles from "./ArrowBack.module.scss";
 import arrowSvg from "../../../assets/img/arrow_back.svg";
 
-export default function ArrowBack({linkTo}) {
+export default function ArrowBack({ linkTo }) {
   return (
     <Link to={linkTo} className={Styles.ArrowBack}>
       <img src={arrowSvg} alt="Go Back" />
