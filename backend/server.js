363d0792ca83e0306f1d950b3a27d74b2bb7da14
AnyaSen <anyasenchikhina18@gmail.com>
2020-07-16 12:11:09 +0300

    Modified files for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("./db/mongoose");
+const dotenv = require("dotenv");
 
 const WordRouter = require("./routers/word-router");
 const UserRouter = require("./routers/user-router");
@@ -11,6 +12,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(WordRouter);
 app.use(UserRouter);
+
+dotenv.config();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../frontend/build"));
