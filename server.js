547bb603dd5b439e8340dd1ddac58e39f3fcdbf9
AnyaSen <anyasenchikhina18@gmail.com>
2020-07-17 11:59:57 +0300

    Made changes for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 
 const WordRouter = require("./routers/word-router");
 const UserRouter = require("./routers/user-router");
+const path = require("path");
 
 const app = express();
 
@@ -18,6 +19,9 @@ dotenv.config();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
 }
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
