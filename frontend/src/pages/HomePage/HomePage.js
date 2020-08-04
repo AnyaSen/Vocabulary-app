8b7353b6e2d22d8bd8def166be9fd72d61f3ada8
AnyaSen <anyasenchikhina18@gmail.com>
2020-08-04 10:58:28 +0300

    Integrated languge context into HomePage

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Styles from "./HomePage.module.scss";
 import vocabularySvg from "../../assets/img/vocabulary.svg";
@@ -8,40 +8,54 @@ import progressSvg from "../../assets/img/progress.svg";
 import Layout from "../../components/Layout/Layout";
 import PageLayout from "../../components/PageLayout/PageLayout";
 import HomePageButton from "../../components/Buttons/HomePageButton";
+import typography from "../../typography/typography.json";
+import { LanguageContext } from "../../contexts/LanguageContext";
 
 export default function HomePage() {
   const name = JSON.parse(localStorage.getItem("userName"));
+
+  const { language } = useContext(LanguageContext);
+
+  const {
+    nice_to_see_you,
+    what_would_you_like_to_do,
+    vocabulary_explanation,
+    progress_explanation,
+    learn_explanation
+  } = typography[language].HomePage;
+
+  const { vocabulary, progress, learn } = typography[language].shared;
 
   return (
     <Layout>
       <PageLayout
         header={`${name},`}
-        headerAfterBreak="Nice to see you here!"
-        subHeader="What would you like to do?"
+        headerAfterBreak={nice_to_see_you}
+        subHeader={what_would_you_like_to_do}
       >
         <div className={Styles.buttonsContainer}>
           <HomePageButton
             imgSrc={vocabularySvg}
             imgAlt="Vocabulary"
             linkTo="/vocabulary"
-            buttonMessage="Vocabulary"
-            buttonDescription="To add and browse words"
+            buttonMessage={vocabulary}
+            buttonDescription={vocabulary_explanation}
           />
 
           <HomePageButton
             imgSrc={progressSvg}
             imgAlt="Progress"
             linkTo="/progress"
-            buttonMessage="Progress"
-            buttonDescription="To see your learning progress"
+            buttonMessage={progress}
+            buttonDescription={progress_explanation}
           />
 
           <HomePageButton
             imgSrc={learnSvg}
             imgAlt="Learn"
             linkTo="/learn"
-            buttonMessage="Learn"
-            buttonDescription="To learn vocabulary"
+            buttonMessage={learn}
+            buttonDescription={learn_explanation}
           />
         </div>
       </PageLayout>
