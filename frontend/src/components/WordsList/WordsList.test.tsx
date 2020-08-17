cca7a227f032297b4e837eb417b3ee1e30766537
AnyaSen <anyasenchikhina18@gmail.com>
2020-08-17 20:34:13 +0300

    Wrote tests

--- frontend/src/components/WordsList/WordsList.test.tsx
+++ frontend/src/components/WordsList/WordsList.test.tsx
@@ -3,27 +3,71 @@ import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 import WordsList from "./WordsList";
-
-// import { ErrorContextProvider } from "../../../contexts/ErrorContext";
-// import { LoadingContextProvider } from "../../../contexts/LoadingContext";
-// import { WordsContextProvider } from "../../../contexts/WordsContext";
-// import { BrowseContextProvider } from "../../../contexts/BrowseContext";
+import { WordsContextProvider } from "../../contexts/WordsContext";
+import { ErrorContextProvider } from "../../contexts/ErrorContext";
+import { LoadingContextProvider } from "../../contexts/LoadingContext";
+import { BrowseContextProvider } from "../../contexts/BrowseContext";
 
 describe("<WordsList />", () => {
   afterEach(cleanup);
 
   const props = {
-    wordsArray: [],
+    wordsArray: [
+      {
+        newlyAdded: false,
+        learning: true,
+        learned: false,
+        _id: "someid",
+        foreignWord: "word",
+        translation: "translation",
+        creator: "myid"
+      },
+      {
+        newlyAdded: true,
+        learning: false,
+        learned: false,
+        _id: "someotherid",
+        foreignWord: "wordtwo",
+        translation: "translationtwo",
+        creator: "mynewid"
+      }
+    ],
+
     noWordsMessage: "No words"
   };
 
+  const createRenderTree = props => (
+    <ErrorContextProvider>
+      <LoadingContextProvider>
+        <WordsContextProvider>
+          <BrowseContextProvider>
+            <WordsList {...props} />
+          </BrowseContextProvider>
+        </WordsContextProvider>
+      </LoadingContextProvider>
+    </ErrorContextProvider>
+  );
+
   const { wordsArray, noWordsMessage } = props;
 
-  test("Should render word pair with correct text, images and not render the edit form.", () => {
-    const { getByAltText, getByText, queryByTestId, getByTestId } = render(
-      <WordsList {...props} />
-    );
+  test("Should render foreignWord and translation for each wordpair and not display any message.", () => {
+    const { getByText, queryByText } = render(createRenderTree(props));
 
-    expect(getByTestId("word-pair-container")).toBeTruthy();
+    wordsArray.forEach((wordPair, index) => {
+      expect(getByText(wordPair.foreignWord)).toBeTruthy();
+      expect(getByText(wordPair.translation)).toBeTruthy();
+      expect(queryByText(noWordsMessage)).toBeFalsy();
+    });
+  });
+
+  test("Should display no words message when the array is empty.", () => {
+    const updatedProps = {
+      ...props,
+      wordsArray: []
+    };
+
+    const { getByText } = render(createRenderTree(updatedProps));
+
+    expect(getByText(noWordsMessage)).toBeTruthy();
   });
 });
