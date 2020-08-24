89329f772f00ba44203d4850c977175c59ed029b
AnyaSen <anyasenchikhina18@gmail.com>
2020-08-24 21:06:15 +0300

    Migrated navbar context to typescript

---
+++ frontend/src/contexts/NavBarConext/NavBarConext.tsx
@@ -0,0 +1,36 @@
+import React, {
+  createContext,
+  useState,
+  ReactNode,
+  Dispatch,
+  ReactElement
+} from "react";
+
+interface NavBarConextType {
+  isBurgerOpen: boolean;
+  setIsBurgerOpen: Dispatch<any>;
+}
+
+interface Props {
+  children: ReactNode;
+}
+
+export const NavBarConext = createContext<NavBarConextType>({
+  isBurgerOpen: false,
+  setIsBurgerOpen: () => {}
+});
+
+export const NavBarConextProvider = ({ children }: Props): ReactElement => {
+  const [isBurgerOpen, setIsBurgerOpen] = useState(false);
+
+  return (
+    <NavBarConext.Provider
+      value={{
+        isBurgerOpen,
+        setIsBurgerOpen
+      }}
+    >
+      {children}
+    </NavBarConext.Provider>
+  );
+};
---
+++ frontend/src/contexts/NavBarConext/index.ts
@@ -0,0 +1,1 @@
+export { NavBarConextProvider, NavBarConext } from "./NavBarConext";
